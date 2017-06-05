fcfbb2001d42a5a394e7af98483bfd5bee020eca
Dvorezky Stefan <Dvorezky Stefan>
2017-06-05 15:42:32 +0200

    add Spring drafts

---
+++ src/main/java/ebi/ensembl/ftpsearchapi/FtpSearchApiApplication.java
@@ -0,0 +1,12 @@
+package ebi.ensembl.ftpsearchapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FtpSearchApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FtpSearchApiApplication.class, args);
+	}
+}
